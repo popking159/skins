b282b76320c8b7c3334ff3607f2759d508bbe3cf
popking159 <47087407+popking159@users.noreply.github.com>
2025-01-21 19:38:06 +0200

    Update installer.sh

--- aglareatv/installer.sh
+++ aglareatv/installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+# Command: wget -q "--no-check-certificate" https://raw.githubusercontent.com/popking159/skins/refs/heads/main/aglareatv/installer.sh -O - | /bin/sh #
 echo "------------------------------------------------------------------------"
 echo "              You are going to install Aglare-FHD                       "
 echo "------------------------------------------------------------------------"
